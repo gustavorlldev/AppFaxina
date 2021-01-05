ce9b35020ebc6e5287ca26c859b9a2b0117574ba
Jorge Luiz <jorgelro@outlook.com.br>
2021-01-05 17:52:56 -0300

    Atualizando página de cadastro

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -24,6 +24,18 @@ export class ProfileComponent implements OnInit {
     })
   }
 
+  files: File[] = [];
+
+	onSelect(event) {
+		console.log(event);
+		this.files.push(...event.addedFiles);
+	}
+
+	onRemove(event) {
+		console.log(event);
+		this.files.splice(this.files.indexOf(event), 1);
+	}
+  
   constructor() { }
 
   ngOnInit() {
