0e35c4c67f1f3a652e398c45dcf603578ec08ab8
Guilherme dos Santos Pereira <guiilherme.pereira27@gmail.com>
2021-01-15 16:50:44 -0300

    animação de cadastro de faxineira completado com sucesso

--- src/app/components/add-faxineira/add-faxineira.component.ts
+++ src/app/components/add-faxineira/add-faxineira.component.ts
@@ -1,10 +1,12 @@
-import {​​​​​ FormControl }​​​​​ from '@angular/forms';
+import {​​​​​ FormControl, FormGroup }​​​​​ from '@angular/forms';
 import {​​​​​ Component, OnInit, Inject }​​​​​ from '@angular/core';
 import {​​​​​ MatDialog }​​​​​ from '@angular/material';
 import {​​​​​ Faxineira }​​​​​ from 'src/app/model/faxineira-model';
 import {​​​​​ AngularFireModule }​​​​​ from '@angular/fire';
 import {​​​​​ AddFaxineiraService }​​​​​ from './add-faxineira.service';
 import {​​​​​ Validators }​​​​​ from '@angular/forms';
+import swal from 'sweetalert';
+
 @Component({​​​​​
   selector: 'app-add-faxineira',
   templateUrl: './add-faxineira.component.html',
@@ -18,24 +20,45 @@ export class AddFaxineiraComponent implements OnInit {​​​​​
   constructor(public dialog: MatDialog, private addFaxineiraService: AddFaxineiraService) {​​​​​
   }​​​​​;
 
+  ngOnInit() {​​​​​
+    // this.novaFaxineira = new Faxineira();
+    this.novaFaxineira = new Faxineira(), new FormGroup({
+      titulo: new FormControl(this.novaFaxineira.titulo, Validators.required),
+      descricao: new FormControl(this.novaFaxineira.descricao, Validators.required),
+      contato: new FormControl(this.novaFaxineira.contato, Validators.required),
+      localidade: new FormControl(this.novaFaxineira.localidade, Validators.required),
+      });
+  }​​​​​
+
   closeFaxineira(): void {​​​​​
     this.dialog.closeAll();
   }​​​​​
 
-  ngOnInit() {​​​​​
-    this.novaFaxineira = new Faxineira();
-  }​​​​​
   adicionarFaxineira() {​​​​​
     if(this.key) {​​​​​
+      {
+        swal(
+          {​​
+          title: 'Erro!',
+          text: 'Por Favor Complete os Campos Obrigatórios!',
+          icon: 'error',
+          }​​)
+      }
     }​​​​​ else {​​​​​
       let addFaxineira = {​​​​​...this.novaFaxineira}​​​​​;
       this.addFaxineiraService.insert(addFaxineira);
     }​​​​​
     this.novaFaxineira = new Faxineira();
     this.dialog.closeAll();
+    swal(
+      {​​
+      title: 'Parabens!',
+      text: 'Anúncio Publicado com Sucesso!',
+      icon: 'success',
+      }​​)
   }​​​​​
   input = new FormControl(null, [Validators.required]);
-  getErrorMessage(){​​​​​
+  getErrorMessage(input){​​​​​
     if(this.input.hasError('required')){​​​​​
       return 'Campo obrigatório';
     }​​​​​;
